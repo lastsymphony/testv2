7de1dd2a2f65088c81d045856727a697caa379ed
lastsymphony <78311812+lastsymphony@users.noreply.github.com>
2021-02-18 19:03:40 +0700

    Add files via upload

---
+++ Fxc7/help.js
@@ -0,0 +1,450 @@
+// menu fitur bot
+const help = (prefix, instagram, yt, name, pushname2, user, limitt, uptime, jam, tanggal) => { 
+	return `	
+
+
+
+┣⊱ ADITYA JATAYU
+
+
+
+
+
+
+
+╭────────────────────────
+┴
+┣⊱ ADITYA JATAYU
+┣⊱ LAST SYMPHONY
+┣⊱ KARO NANGIS -_- 
+┬
+╰────────────────────────
+\`\`\`HARAP BACA NOTE DIBAWAH SEBELUM\`\`\`
+\`\`\`MENGGUNAKAN BOT\`\`\`
+
+╭──────「 *REGULATION ${name}* 」
+┴
+┣⊱  \`\`\`NAMA USER:\`\`\` *${pushname2}*
+┣⊱  \`\`\`VERIVICATION:\`\`\` ✅
+┣⊱  \`\`\`LIMIT:\`\`\` *${limitt} perhari*
+┣⊱  \`\`\`AKTIF:\`\`\` ${kyun(uptime)}
+┣⊱  \`\`\`JAM:\`\`\` *${jam} WIB*
+┣⊱  \`\`\`TANGGAL:\`\`\` *${tanggal}*
+┣⊱  \`\`\`VERSION:\`\`\` *6.5.0*
+┣⊱  \`\`\`USER TERDAFTAR:\`\`\` *${user.length} User*
+┣⊱  ❌ *SPAM*
+┣⊱  ❌ *CALL & VC*
+┣⊱  \`\`\`Melanggar??\`\`\` *Banned + Out Group*
+┬
+╰────────────────────────
+
+
+╭──────「 *ABOUT ${name}* 」
+┴
+│➻ *${prefix}report lapor bug*
+│➻ *${prefix}info*
+│➻ *${prefix}donasi*
+│➻ *${prefix}owner*
+│➻ *${prefix}speed*
+│➻ *${prefix}daftar*
+│➻ *${prefix}limit*
+│➻ *${prefix}totaluser*
+│➻ *${prefix}blocklist*
+│➻ *${prefix}banlist*
+│➻ *${prefix}premiumlist*
+│➻ *${prefix}bahasa*
+┬
+╰────────────────────────
+
+
+╭──────「 *MEDIA DOWNLOADER* 」
+┴
+│➻ *${prefix}tiktokstalk username*
+│➻ *${prefix}igstalk aditya jatayu*
+│➻ *${prefix}instavid link valid*
+│➻ *${prefix}instaimg link valid*
+│➻ *${prefix}instastory username*
+│➻ *${prefix}ssweb url*
+│➻ *${prefix}url2img Url*
+│➻ *${prefix}tiktok*
+│➻ *${prefix}fototiktok*
+│➻ *${prefix}kbbi*
+│➻ *${prefix}wait*
+│➻ *${prefix}trendtwit*
+│➻ *${prefix}google berita terkini*
+┬
+╰────────────────────────
+
+
+╭──────「 *CREATOR MENU* 」
+┴
+│➻ *${prefix}quotemaker tx/wtrmk/tema*
+│➻ *${prefix}nulis nama/kelas/text*
+│➻ *${prefix}croman dan Friend*
+│➻ *${prefix}slide BOT WA*
+├───────────────────
+│➻ *${prefix}tahta*
+│➻ *${prefix}cglass*
+│➻ *${prefix}cstyle*
+│➻ *${prefix}cgame*
+│➻ *${prefix}clove7*
+│➻ *${prefix}cparty*
+│➻ *${prefix}csky*
+│➻ *${prefix}tts id Haii*
+│➻ *${prefix}ttp* maintence
+│➻ *${prefix}cpaper*
+├───────────────────
+│➻ *${prefix}stiker* maintence
+│➻ *${prefix}gifstiker* maintence
+│➻ *${prefix}toimg*
+│➻ *${prefix}img2url*
+│➻ *${prefix}tomp3*
+│➻ *${prefix}ocr*
+┬
+╰──────────────────────────
+
+
+╭───────「 *GROUP ONLY* 」
+┴
+│➻ *${prefix}modeanime On/Off*
+│➻ *${prefix}neonime naruto*
+│➻ *${prefix}naruto*
+│➻ *${prefix}minato*
+│➻ *${prefix}boruto*
+│➻ *${prefix}hinata*
+│➻ *${prefix}sakura*
+│➻ *${prefix}sasuke*
+│➻ *${prefix}toukachan*
+│➻ *${prefix}rize*
+│➻ *${prefix}akira*
+│➻ *${prefix}itori*
+│➻ *${prefix}kurumi*
+│➻ *${prefix}miku*
+│➻ *${prefix}anime*
+│➻ *${prefix}animecry*
+│➻ *${prefix}animekiss*
+┬
+╰───────────────────────
+
+
+╭───────「 *GROUP ONLY* 」
+┴
+│➻ *${prefix}welcome On/Off*
+│➻ *${prefix}grup buka/tutup*
+│➻ *${prefix}ownergrup*
+│➻ *${prefix}setpp*
+│➻ *${prefix}infogc*
+│➻ *${prefix}add 628xxxxxxxxxx*
+│➻ *${prefix}kick @mentioned*
+│➻ *${prefix}kicktime @mentioned*
+│➻ *${prefix}promote @mentioned*
+│➻ *${prefix}demote @mentioned*
+│➻ *${prefix}setname*
+│➻ *${prefix}setdesc*
+│➻ *${prefix}linkgrup*
+│➻ *${prefix}tagme*
+│➻ *${prefix}hidetag*
+│➻ *${prefix}tagall*
+│➻ *${prefix}mentionall*
+│➻ *${prefix}fitnah @mentioned/isi/balasan*
+│➻ *${prefix}listadmin*
+│➻ *${prefix}daftarmember*
+┬
+╰────────────────────────
+
+
+╭───────「 *GROUP ONLY* 」
+┴───────「 *RANDOM MENU* 」
+│➻ *${prefix}nsfw On/Off*
+│➻ *${prefix}loli*
+│➻ *${prefix}randomdoujin*
+│➻ *${prefix}cosplay*
+│➻ *${prefix}loli 2*
+│➻ *${prefix}milf*
+│➻ *${prefix}milfsan*
+│➻ *${prefix}nsfwblowjob*
+│➻ *${prefix}nsfwneko*
+│➻ *${prefix}nsfwtrap*
+│➻ *${prefix}hentai*
+│➻ *${prefix}simih On/Off*
+│➻ *${prefix}yuri*
+│➻ *${prefix}randomsongs*
+┬
+╰────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}anjing*
+│➻ *${prefix}kucing*
+│➻ *${prefix}testime*
+│➻ *${prefix}hilih*
+│➻ *${prefix}apakah*
+│➻ *${prefix}kapankah*
+│➻ *${prefix}bisakah*
+│➻ *${prefix}rate*
+│➻ *${prefix}watak*
+│➻ *${prefix}hobby*
+│➻ *${prefix}infogempa*
+│➻ *${prefix}infonomor*
+│➻ *${prefix}quotes*
+│➻ *${prefix}truth*
+│➻ *${prefix}dare*
+│➻ *${prefix}katabijak*
+│➻ *${prefix}fakta*
+│➻ *${prefix}darkjokes*
+│➻ *${prefix}bucin*
+│➻ *${prefix}pantun*
+│➻ *${prefix}katacinta*
+│➻ *${prefix}jadwaltvnow*
+│➻ *${prefix}hekerbucin*
+│➻ *${prefix}katailham*
+┬
+╰────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}jarak Banyuwangi/Surabaya*
+│➻ *${prefix}translate en/Apa kabar?*
+│➻ *${prefix}pasangan last/Iriene*
+│➻ *${prefix}gantengcek Farhan*
+│➻ *${prefix}cantikcek last*
+│➻ *${prefix}artinama last*
+│➻ *${prefix}persengay Topan*
+│➻ *${prefix}pbucin last*
+│➻ *${prefix}bpfont last*
+│➻ *${prefix}textstyle *
+│➻ *${prefix}jadwaltv antv*
+│➻ *${prefix}lirik melukis senja*
+│➻ *${prefix}chord Melukis senja*
+│➻ *${prefix}wiki Adolf Hitler*
+│➻ *${prefix}brainly pertanyaan*
+│➻ *${prefix}resepmasakan rawon*
+│➻ *${prefix}map Banyuwangi*
+│➻ *${prefix}film Fast and Farious*
+│➻ *${prefix}pinterest gambar kucing*
+│➻ *${prefix}infocuaca Banyuwangi*
+│➻ *${prefix}jamdunia Banyuwangi*
+│➻ *${prefix}mimpi Ular*
+│➻ *${prefix}infoalamat jalan Banyuwangi*
+│➻ *${prefix}playstore WhatsApp*
+┬
+╰───────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}readmore*
+│➻ *${prefix}puisiimg*
+│➻ *${prefix}asupan*
+│➻ *${prefix}tebakgambar*
+│➻ *${prefix}caklontong*
+│➻ *${prefix}family100*
+│➻ *${prefix}memeindo*
+│➻ *${prefix}kalkulator 13*12*
+│➻ *${prefix}moddroid lightroom*
+│➻ *${prefix}happymod lightroom*
+┬
+╰────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}randomKPOP*
+│➻ *${prefix}cersex*
+│➻ *${prefix}randombokep*
+│➻ *${prefix}pornhub stepMoms*
+│➻ *${prefix}xvideos japan*
+│➻ *${prefix}nekopoi*
+┬
+╰────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}jadwalsholat Banyuwangi*
+│➻ *${prefix}quran*
+│➻ *${prefix}quranlist*
+│➻ *${prefix}quransurah 1*
+┬
+╰────────────────────────
+
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}spamcall 083xxxxxxxxx*
+│➻ *${prefix}spamgmail contoh@gmail.com*
+┬
+╰────────────────────────
+
+
+╭─────────「 *OWNER ONLY* 」
+┴
+│➻ *${prefix}addprem mentioned*
+│➻ *${prefix}removeprem mention*
+│➻ *${prefix}setmemlimit*
+│➻ *${prefix}setlimit*
+│➻ *${prefix}setreply*
+│➻ *${prefix}setprefix*
+│➻ *${prefix}setnamebot*
+│➻ *${prefix}setppbot*
+│➻ *${prefix}bc*
+│➻ *${prefix}bcgc*
+│➻ *${prefix}ban*
+│➻ *${prefix}unban*
+│➻ *${prefix}block*
+│➻ *${prefix}unblock*
+│➻ *${prefix}clearall*
+│➻ *${prefix}delete*
+│➻ *${prefix}clone*
+│➻ *${prefix}getses*
+│➻ *${prefix}leave*
+│➻ *${prefix}addbucin
+┬
+╰────────────────────────
+
+
+╭────────「 *PREMIUM ONLY* 」
+┴
+│➻ *${prefix}playmp3 menepi*
+│➻ *${prefix}fb link video*
+│➻ *${prefix}snack link snack video*
+│➻ *${prefix}ytmp3 link yt*
+│➻ *${prefix}ytmp4 link yt*
+│➻ *${prefix}joox Monolog Pamungkas*
+│➻ *${prefix}smule Link Video Smule*
+┬
+╰────────────────────────
+
+
+╭─────「 *SUPPORT ${name}* 」
+┴
+│➲ *ADITYA JATAYU*
+│➲ *LAST SYMPHONY*
+│➲ *A*
+│➲ *D*
+│➲ *MY TEAM FXC7 BOT*
+│➲ *CONTENT CREATOR BOT WHATSAPP*
+┬
+╰────────────────────────`
+}
+
+exports.help = help
+
+// penghitung aktif bot
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+  return `*${pad(hours)} Jam ${pad(minutes)} Menit ${pad(seconds)} Detik*`
+}
+
+// info bot 
+const bottt = (prefix) => {
+return `
+\`\`\`Untuk Sekarang Bot Hanya Bisa Digunakan Di Group Karna\`\`\` *BOT JOMBLO*\n\n *Mohon Pengertiannya*
+Sekali Lagi Maaf
+
+Jika Bot Ini Ada Di Grup Anda Admin Grup Suruh Aktifkan Bot Dengan Cara ${prefix}bott aktif
+`
+}
+exports.bottt = bottt
+// donasi menu
+const donasi = (name) => { 
+	return `       
+╭─────「 *DONASI SEIKHLASNYA* 」
+┴
+│√ *PULSA: 089670394574*
+│√ *OVO : 089670394574*
+│
+│\`\`\`Pulsa\`\`\`
+│*15k Invite Bot Ke Grup selama  bulan*
+│
+│\`\`\`OVO\`\`\`
+│*10k Invite Bot Ke Group Selama  Bulan*
+┬
+╰──────「 *BY ${name}* 」
+
+Untuk Kelangsungan Hidup Bot Karna '''BOT JOMBLO'''
+`
+}
+exports.donasi = donasi
+
+// bahasa list
+const bahasa = (prefix) => {
+return `
+List Bahasa Untuk Command *${prefix}tts*
+
+  af: Afrikaans
+  sq: Albanian
+  ar: Arabic
+  hy: Armenian
+  ca: Catalan
+  zh: Chinese
+  zh-cn: Chinese (Mandarin/China)
+  zh-tw: Chinese (Mandarin/Taiwan)
+  zh-yue: Chinese (Cantonese)
+  hr: Croatian
+  cs: Czech
+  da: Danish
+  nl: Dutch
+  en: English
+  en-au: English (Australia)
+  en-uk: English (United Kingdom)
+  en-us: English (United States)
+  eo: Esperanto
+  fi: Finnish
+  fr: French
+  de: German
+  el: Greek
+  ht: Haitian Creole
+  hi: Hindi
+  hu: Hungarian
+  is: Icelandic
+  id: Indonesian
+  it: Italian
+  ja: Japanese
+  ko: Korean
+  la: Latin
+  lv: Latvian
+  mk: Macedonian
+  no: Norwegian
+  pl: Polish
+  pt: Portuguese
+  pt-br: Portuguese (Brazil)
+  ro: Romanian
+  ru: Russian
+  sr: Serbian
+  sk: Slovak
+  es: Spanish
+  es-es: Spanish (Spain)
+  es-us: Spanish (United States)
+  sw: Swahili
+  sv: Swedish
+  ta: Tamil
+  th: Thai
+  tr: Turkish
+  vi: Vietnamese
+  cy: Welsh
+`
+}
+exports.bahasa = bahasa
+
+// Limit
+const limitend = (pushname2) => {
+        return`*maaf ${pushname2} limit hari ini habis*\n*limit di reset setiap jam 00:00 WIB TENGAH MALAM*`
+}
+
+const limitcount = (limitCounts) => {
+        return`
+Limit Kamu: ${limitCounts}
+`
+}
+
+exports.limitend = limitend
+exports.limitcount = limitcount
---
+++ Fxc7/loli2.js
@@ -0,0 +1,73 @@
+[
+	 {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/a165cb09b6/1812684-16"
+    },
+     {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/a4a8284760/1811066-2"
+    },
+    {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/cba054cb36/1801388-3"
+    },
+    {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/6b1b161a43/1797801-4"
+    },
+    {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/eae38e662e/1797801-21"
+    },
+    {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/cfd88a66ef/1795630-3"
+    },
+    {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/09a7937a95/1795630-4"
+    },
+    {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/dc75406a32/1793961-9"
+    },
+    {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/20da5ca0ad/1793961-14"
+    },
+    {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/ab0e6a2cc6/1793961-11"
+    },
+    {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/d68cec2160/1793961-5"
+    },
+    {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/04c46e0467/1785458-1"
+    },
+    {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/2a100388d3/1785458-5"
+    },
+     {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/be923d2b19/1785458-11"
+    },
+    {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/2a37a2b008/1785459-9"
+    },
+    {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/33ffb78df4/1785455-2"
+    },
+    {
+	"teks": "*Silahkan Diambil Asupan Lolinya*",
+	"image": "https://e-hentai.org/s/29e79e2b8d/1761200-2"
+    },
+    
+   
+   
+]
